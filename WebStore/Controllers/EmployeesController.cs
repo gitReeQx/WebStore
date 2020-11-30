d45938c2a231df62d081e3c981326e1cbd4e13ce
gitReeQx <mreeqx@gmail.com>
2020-11-30 20:42:04 +0800

    Добавлена возможность удалять и редактировать сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -24,5 +24,33 @@ namespace WebStore.Controllers
 
             return NotFound();
         }
+
+        public IActionResult Edit(int id)
+        {
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee is not null)
+                return View(employee);
+
+            return NotFound();
+        }
+
+        public IActionResult SaveChanges(Employee _employee)
+        {
+            int index = employees.IndexOf(employees.Where(n => n.Id == _employee.Id).FirstOrDefault());
+            employees[index] = _employee;
+            return RedirectToAction("Details", new { id = _employee.Id });
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee is not null)
+            {
+                employees.Remove(employee);
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
     }
 }
