fe6d9e647eeae0d354060ca0f222793eb4726067
gitReeQx <mreeqx@gmail.com>
2020-12-14 09:05:27 +0800

    Секции и бренды в памяти

--- WebStore/Data/TestData.cs
+++ WebStore/Data/TestData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities;
 using WebStore.Models;
 
 namespace WebStore.Data
@@ -13,6 +14,16 @@ namespace WebStore.Data
             new Employee { Id = 1, FirstName = "Евгений", LastName = "Кузнецов", Patronymic = "Александрович", Age = 32 },
             new Employee { Id = 2, FirstName = "Евгений", LastName = "Беляев", Patronymic = "Юрьевич", Age = 35 },
             new Employee { Id = 3, FirstName = "Сергей", LastName = "Чурствин", Patronymic = "Юрьевич", Age = 33 }
+        };
+
+        public static IEnumerable<Section> Sections { get; } = new[]
+        {
+            new Section{Id = 1, Name = "Спорт", Order = 1},
+        };
+
+        public static IEnumerable<Brand> Brands { get; } = new[]
+        {
+            new Brand{Id = 1, Name = "Adidas", Order = 1},
         };
     }
 }
