7b6db63c84ffa2aafb6f2a45cbb92002ad8128e9
gitReeQx <mreeqx@gmail.com>
2020-12-14 08:42:39 +0800

    Создание базовых сущностей

---
+++ WebStore.Domain/Entities/Base/Interface/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace WebStore.Domain.Entities.Base.Interface
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entities/Base/Interface/IOrderedEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace WebStore.Domain.Entities.Base.Interface
+{
+    public interface IOrderedEntity : IEntity
+    {
+        int Order { get; set; }
+    }
+}
