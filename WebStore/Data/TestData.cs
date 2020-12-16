913bd70e3eac4dda76228db4c98031bc642a20eb
gitReeQx <mreeqx@gmail.com>
2020-12-16 09:31:04 +0800

    Сущность товар

--- WebStore/Data/TestData.cs
+++ WebStore/Data/TestData.cs
@@ -30,5 +30,11 @@ namespace WebStore.Data
             new Brand{Id = 1, Name = "Adidas", Order = 1},
             new Brand{Id = 2, Name = "Nike", Order = 2},
         };
+
+        public static IEnumerable<Product> Products { get; } = new[]
+        {
+            new Product{Id = 1, Name = "Кроссовки", Order = 1, ImageUrl = "image9.jpg", BrandId = 1, SectionId = 2, Price = 9000},
+            new Product{Id = 2, Name = "Часы", Order = 2, ImageUrl = "image10.jpg", SectionId = 4, Price = 12000},
+        };
     }
 }
