80e1b694b6da0281ad72d7f5f7d1bd414db8348d
gitReeQx <mreeqx@gmail.com>
2020-12-16 09:55:49 +0800

    Модели-представления каталога товаров

---
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base.Interface;
+
+namespace WebStore.ViewModels
+{
+    public class ProductViewModel : INamedEntity, IOrderedEntity
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public int Order { get; set; }
+
+        public string ImageUrl;
+
+        public decimal Price;
+    }
+}
