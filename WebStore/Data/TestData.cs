d7757d4cc5795cd471f8edb6bf64cad190c45ca3
gitReeQx <mreeqx@gmail.com>
2020-12-16 09:10:50 +0800

    Формирование и вывод брендов

--- WebStore/Data/TestData.cs
+++ WebStore/Data/TestData.cs
@@ -19,15 +19,16 @@ namespace WebStore.Data
         public static IEnumerable<Section> Sections { get; } = new[]
         {
             new Section{Id = 1, Name = "Спорт", Order = 1},
-            new Section{Id = 1, Name = "Обувь", Order = 2, ParentId = 1},
-            new Section{Id = 1, Name = "Аксессуары", Order = 3},
-            new Section{Id = 1, Name = "Часы", Order = 4, ParentId = 3},
-            new Section{Id = 1, Name = "Браслеты", Order = 5, ParentId = 3},
+            new Section{Id = 2, Name = "Обувь", Order = 2, ParentId = 1},
+            new Section{Id = 3, Name = "Аксессуары", Order = 3},
+            new Section{Id = 4, Name = "Часы", Order = 4, ParentId = 3},
+            new Section{Id = 5, Name = "Браслеты", Order = 5, ParentId = 3},
         };
 
         public static IEnumerable<Brand> Brands { get; } = new[]
         {
             new Brand{Id = 1, Name = "Adidas", Order = 1},
+            new Brand{Id = 2, Name = "Nike", Order = 2},
         };
     }
 }
--- WebStore/ViewModels/SectionViewModel.cs
+++ WebStore/ViewModels/SectionViewModel.cs
@@ -12,7 +12,7 @@ namespace WebStore.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public List<SectionViewModel> ChildSection { get; set; }
+        public List<SectionViewModel> ChildSection { get; set; } = new();
 
         public SectionViewModel ParentSection { get; set; }
     }
