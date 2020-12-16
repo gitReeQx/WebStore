cc39c4f92993358bfcd2eda828d08bec68e3cb77
gitReeQx <mreeqx@gmail.com>
2020-12-16 12:58:29 +0800

    Модель БД

--- WebStore.Domain/Entities/Base/NamedEntity.cs
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -1,9 +1,11 @@
-﻿using WebStore.Domain.Entities.Base.Interface;
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base.Interface;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class NamedEntity : Entity, INamedEntity
     {
+        [Required]
         public string Name { get; set; }
     }
 }
--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,10 +1,16 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interface;
 
 namespace WebStore.Domain.Entities
 {
+    //[Table("Brands")]
     public class Brand: NamedEntity, IOrderedEntity
     {
+        //[Column("BrandOrder")]
         public int Order { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
