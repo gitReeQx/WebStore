3eb0251ee1bea1acca9f1570a4c76465e1528207
gitReeQx <mreeqx@gmail.com>
2020-12-25 15:13:04 +0800

    Авторизация в области администратора

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class CatalogController : Controller
     {
+        private readonly IProductData productData;
+
+        public CatalogController(IProductData _productData)
+        {
+            productData = _productData;
+        }
+
         public IActionResult Index()
         {
             return View();
--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
