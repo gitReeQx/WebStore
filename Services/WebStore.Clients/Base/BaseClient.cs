31b5bb8802a39712b4f0378933991efae89bc449
gitReeQx <mreeqx@gmail.com>
2021-01-23 15:14:39 +0800

    Клиент тестового WebAPI

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected string Address { get; }
+
+        protected HttpClient Http { get; }
+
+        protected BaseClient (HttpClient Client, string ServiceAddress)
+        {
+            Http = Client;
+            Address = ServiceAddress;
+        }
+    }
+}
