37eb3584448b73d176df7cf7827e9e37a0da2a0e
gitReeQx <mreeqx@gmail.com>
2021-01-27 20:29:49 +0800

    Расширение функциональности базового клиента

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
 {
@@ -22,6 +23,38 @@ namespace WebStore.Clients.Base
                     Accept = { new MediaTypeWithQualityHeaderValue("application/json") }
                 }
             };
+        }
+
+        protected T Get<T>(string url) => GetAsync<T>(url).Result;
+
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await Http.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await Http.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await Http.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await Http.DeleteAsync(url);
+            return response;
         }
     }
 }
