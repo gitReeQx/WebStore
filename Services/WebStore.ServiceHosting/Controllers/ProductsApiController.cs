ba8229a41b0029a196e73df54687591fc7037e07
gitReeQx <mreeqx@gmail.com>
2021-01-29 12:09:50 +0800

    Загатовка контроллера WebAPI каталога товаров

---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase, IProductData
+    {
+        private readonly IProductData productData;
+
+        public ProductsApiController(IProductData ProductData) => productData = ProductData;
+
+        public void AddProduct(Product product)
+        {
+            productData.AddProduct(product);
+        }
+
+        public bool DeleteProduct(int id)
+        {
+            return productData.DeleteProduct(id);
+        }
+
+        [HttpGet("brands/{id}")]
+        public Brand GetBrandById(int id)
+        {
+            return productData.GetBrandById(id);
+        }
+
+        [HttpGet("brands")]
+        public IEnumerable<Brand> GetBrands()
+        {
+            return productData.GetBrands();
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProductById(int id)
+        {
+            return productData.GetProductById(id);
+        }
+
+        [HttpPost]
+        public IEnumerable<Product> GetProducts([FromBody]ProductFilter Filter = null)
+        {
+            return productData.GetProducts(Filter);
+        }
+
+        [HttpGet("sections/{id}")]
+        public Section GetSectionById(int id)
+        {
+            return productData.GetSectionById(id);
+        }
+
+        [HttpGet("sections")]
+        public IEnumerable<Section> GetSections()
+        {
+            return productData.GetSections();
+        }
+    }
+}
--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using WebStore.Clients.Employees;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
@@ -66,7 +67,7 @@ namespace WebStore
                 opt.SlidingExpiration = true;
             });
 
-            services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
+            services.AddTransient<IEmployeesData, EmployeesClient>();
             services.AddTransient<IProductData, SQLProductData>();
             services.AddScoped<ICartService, InCookiesCartService>();
             services.AddScoped<IOrderService, SQLOrderService>();
