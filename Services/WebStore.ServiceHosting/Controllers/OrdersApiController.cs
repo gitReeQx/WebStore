67d9f66a16b013ee1178fc8d741ed64ad8e36ca7
gitReeQx <mreeqx@gmail.com>
2021-01-30 13:55:04 +0800

    WebAPI сервиса заказов

---
+++ Services/WebStore.ServiceHosting/Controllers/OrdersApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebStore.Domain.DTO.Orders;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Orders)]
+    [ApiController]
+    public class OrdersApiController : ControllerBase, IOrderService
+    {
+        private readonly IOrderService orderService;
+        private readonly Logger<OrdersApiController> logger;
+
+        public OrdersApiController(IOrderService OrderService, Logger<OrdersApiController> Logger)
+        {
+            orderService = OrderService;
+            logger = Logger;
+        }
+
+        public Task<OrderDTO> CreateOrder(string UserName, [FromBody] CreateOrderModel OrderModel)
+        {
+            logger.LogInformation("Формирование заказа для пользователя {0}", UserName);
+            return orderService.CreateOrder(UserName, OrderModel);
+        }
+
+        public Task<OrderDTO> GetOrderById(int id)
+        {
+            return orderService.GetOrderById(id);
+        }
+
+        public Task<IEnumerable<OrderDTO>> GetUserOrders(string UserName)
+        {
+            return orderService.GetUserOrders(UserName);
+        }
+    }
+}
