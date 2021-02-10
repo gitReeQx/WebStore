4ade71610625549ff87eabd80d23abcc3d75385d
gitReeQx <mreeqx@gmail.com>
2021-02-10 20:02:18 +0800

    Конфигурация системы логирования

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -20,6 +20,13 @@ namespace WebStore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(host => host.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(host => host.UseStartup<Startup>())
+            //.ConfigureLogging((host, log) => log
+            //    .ClearProviders()
+            //    .AddEventLog(opt => opt.LogName = "WebStore.log")
+            //    .AddFilter("Microsoft.Hosting", LogLevel.Error)
+            //    .AddFilter((category, level) => !(category.StartsWith("Microsoft") && level >= LogLevel.Warning))
+            // )
+            ;
     }
 }
