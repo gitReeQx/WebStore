11c22ec7772257b4dc09665dfc08331a74049a4d
gitReeQx <mreeqx@gmail.com>
2021-02-10 21:43:53 +0800

    Метод-расширение для добавления логирования в log4net

---
+++ Common/WebStore.Logger/Log4NetLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Logger
+{
+    class Log4NetLogger
+    {
+    }
+}
